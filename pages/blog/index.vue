873b44cc029f64a3d7a0159ec2d5b9d229d8b2ec
Vicente Bermúdez <stealyerms@gmail.com>
2022-09-17 11:34:31 -0300

    Initial commit

---
+++ pages/blog/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { Post } from '~/types';
+import type { Strapi4Response } from '@nuxtjs/strapi';
+
+const { find } = useStrapi4();
+
+const posts = ref<Post[]>([]);
+try {
+  const response = await find<Strapi4Response<Post>>('articles', {
+    sort: 'publishedAt',
+    populate: ['image', 'author'],
+  });
+
+  posts.value = response.data;
+} catch (e) {
+  console.log(e);
+}
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl mb-2 mt-2 font-semibold">Blog</h1>
+    <h2 class="text-lg mb-8 font-normal text-gray-600 dark:text-gray-400">
+      Latest Posts
+    </h2>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <BlogPostItem v-for="post in posts" :key="post.id" :post="post" />
+    </div>
+  </div>
+</template>
---
+++ pages/swiper.vue
@@ -0,0 +1,91 @@
+<script setup>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Navigation, Pagination, A11y } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+definePageMeta({
+  fullWidth: true,
+});
+
+const onSwiper = (swiper) => {
+  console.log(swiper);
+};
+
+const onSlideChange = () => {
+  console.log("slide change");
+};
+
+const modules = [Navigation, Pagination, A11y];
+</script>
+
+<template>
+  <div>
+    <swiper
+      :modules="modules"
+      :slides-per-view="1"
+      :space-between="50"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+      class="relative"
+    >
+      <swiper-slide v-for="i in 5" :key="i">
+        <HeroSection
+          :title="`Slide ${i}`"
+          description="Lorem ipsum"
+          :image="`https://picsum.photos/1920/1080?random=${i}`"
+          :button="{ text: 'Get Started', action: '#' }"
+        />
+      </swiper-slide>
+    </swiper>
+
+    <swiper
+      :modules="modules"
+      navigation
+      :pagination="{ clickable: true }"
+      :slides-per-view="1"
+      :space-between="50"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+    >
+      <swiper-slide v-for="i in 5" :key="i">
+        <HomeQuote class="text-center" :title="`Quote ${i}`" description="Lorem ipsum" />
+      </swiper-slide>
+    </swiper>
+
+    <div class="custom-swiper relative mt-10 bg-blue-50">
+      <button
+        class="swiper-btn-prev btn btn-primary rounded-full z-10 absolute top-1/2 left-2 transform -translate-y-1/2"
+      >
+        Prev
+      </button>
+      <button
+        class="swiper-btn-next btn btn-primary rounded-full z-10 absolute top-1/2 right-2 transform -translate-y-1/2"
+      >
+        Next
+      </button>
+
+      <swiper
+        :modules="modules"
+        :navigation="{
+          nextEl: '.swiper-btn-next',
+          prevEl: '.swiper-btn-prev',
+        }"
+        :pagination="{ clickable: true }"
+        :slides-per-view="1"
+        :space-between="50"
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+      >
+        <swiper-slide v-for="i in 5" :key="i">
+          <HomeQuote
+            class="text-center"
+            :title="`Quote ${i}`"
+            description="Lorem ipsum"
+          />
+        </swiper-slide>
+      </swiper>
+    </div>
+  </div>
+</template>
